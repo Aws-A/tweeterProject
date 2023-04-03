a46627346d9000a2d1fdaf4a23885f13823688a3
Aws-star <awsalrahim@yahoo.com>
2023-04-02 23:09:52 -0400

    Updating with screenshots

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,41 +4,75 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+$(document).ready(function() {
+  //Load Tweets
+  const loadTweets = function () {
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function (data) {
+        console.log('Success: ', data);
+        renderTweets(data);
+      });
   }
-];
 
-const renderTweets = function () {
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-}
+  // Create tweets
+  const createTweetElement = function (key) {
+    const $tweet = `<article class="containerTweets">
+      <header class="headerShow">
+        <div class="profile">
+          <img src="${key['user']['avatars']}" style="height: 80%; padding-right: 10%"> 
+          <p class="profileName"> ${key['user']['name']} </p>
+        </div>
+        <P class="account"> ${key['user']['handle']} </P>
+      </header>
+      <container id="tweets-container"> ${escape(key['content']['text'])} </container>
+      <hr>
+      <footer class="footerShow">
+        <p class="footerP"> ${timeago.format(key['created_at'])} </p>
+        <div class="icons"> 
+          <i class="fa-solid fa-flag footIc"></i> 
+          <i class="fa-solid fa-retweet footIc"></i>
+          <i class="fa-solid fa-heart footIc"></i>
+        </div>
+      </footer>
+    </article>`
+    return $tweet;
+  }
 
-const createTweetElement = function () {
-
-  const $tweet = $(`<article class="tweet">Hello world</article>`);
-
-  return $tweet;
-
-}
-
-renderTweets(data);
+  // Render Tweets
+  const renderTweets = function (data) {
+    for (let i = 0; i < data.length; i++) {
+      let key =  data[i];
+      let $tweet = createTweetElement(key);
+      $('#allTweets').prepend($tweet);
+    }
+  }
+  //Check the input to be 140 or less or empty
+  $( "#form" ).submit(function( event ) {
+    event.preventDefault();
+    if ($("#tweet-text").val().length === 0) {
+      $('.error-message').show();
+      $('.error-message').text("The tweet should not be empty!");
+    } else if ($("#tweet-text").val().length <= 140) {
+      $('.error-message').text("");
+      $('.error-message').hide();
+      $.post("/tweets",  $( this ).serialize(),   function(response, status)  {
+      // Clear text area
+      $("#tweet-text").val(""); 
+      // Reload Tweets
+      $('#allTweets').empty();
+      $(".counter").val(140);
+      loadTweets();
+      });
+    } else {
+    $('.error-message').show();
+    $('.error-message').text("The limit is 140 characters!");
+    }
+  });
+  loadTweets();
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,24 +2,13 @@ $(document).ready(function() {
   // --- our code goes here ---
   $("#tweet-text").on('input', function() {
    const counter = $(this).val().length;
-    charRemaining = 140 - counter;
-    if (charRemaining < 0) {
-      $(".counter").css("color", "red");
-
-    }
-   $(".counter").text(charRemaining);   
-  });
-
-
-  $( "#form" ).submit(function( event ) {
-    event.preventDefault();
-    if ($("#tweet-text").val().length <= 14) {
-      $.post("/tweets",  $( this ).serialize(),   function(response, status){
-        alert("Data: " + response + "\nStatus: " + status);
-      });
-    } else {
-      alert("The limit is 140 characters!")
-    }
+   charRemaining = 140 - counter;
+   if (charRemaining < 0) {
+    $(".counter").css("color", "red");
+   } else {
+    $(".counter").css("color", "black");
+   }
+    $(".counter").text(charRemaining);   
   });
 });
 
